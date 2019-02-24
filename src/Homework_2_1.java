1783c6d6378cf7cf2184dccb1a6cfca15f8cf5bd
OneAndrew <a.tarasyuk@ukr.net>
2019-02-25 00:04:32 +0200

    add one more task

---
+++ src/Homework_2_1.java
@@ -0,0 +1,13 @@
+public class Homework_2_1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("b=" + b + " and " + "a=" + a);
+
+    }
+}
